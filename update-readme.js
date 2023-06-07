284d518d862858363fadf815514d9c60dcf02245
flavacoin <79034517+flavacoin@users.noreply.github.com>
2023-06-07 01:37:56 +0100

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -7,14 +7,8 @@ const myVariable = 'Hello, World!';
 // Read the template file
 const template = fs.readFileSync('README-template.md', 'utf8');
 
-// Log the template content
-console.log('Template:', template);
-
 // Render the template with variables
-const rendered = ejs.render(template, { myVariable });
-
-// Log the rendered content
-console.log('Rendered:', rendered);
+const rendered = ejs.render(template, { myVariable }, { delimiter: '%' });
 
 // Write the updated content to README.md
 fs.writeFileSync('README.md', rendered, 'utf8');
